8a4cb960c74f7a49d9f3a8eb24e62ec0f33a3067
James Njoroge <98014792+devjamesnjoroge@users.noreply.github.com>
2023-10-15 20:12:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ document.querySelector(".menu").addEventListener("click", function(){
         menu.classList.add("collapse")
     }
 })
+
+let logo = document.querySelector(".logo")
+
+logo.addEventListener("click", ()=>{
+    location.reload()
+})
+
+function deleteAndRewrite() {
+    var outputElement = document.getElementById('output');
+    var text = outputElement.textContent;
+    var length = text.length;
+
+    // Delete one letter at a time
+    for (let i = length; i >= 0; i--) {
+        setTimeout(function () {
+            outputElement.innerHTML = text.substring(0, i);
+        }, (length - i) * 1000); // 3 seconds for each letter
+    }
+
+    // Rewrite one letter at a time
+    for (let i = 0; i <= length; i++) {
+        setTimeout(function () {
+            outputElement.innerHTML = text.substring(0, i);
+        }, (length + i) * 1000); // 3 seconds for each letter
+    }
+}
+
+
+setTimeout(()=>{
+    deleteAndRewrite()
+}, 15000)
